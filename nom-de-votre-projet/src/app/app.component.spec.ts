a123eedf90e91164f28031cf1f33c75b246dc5b5
momoikich <mohamed.ikich@etu.toulouse-inp.fr>
2024-01-11 18:46:37 +0100

    supprimer l'application

--- nom-de-votre-projet/src/app/app.component.spec.ts
+++ nom-de-votre-projet/src/app/app.component.spec.ts
@@ -14,12 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  
+  it(`should have the 'nom-de-votre-projet' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('nom-de-votre-projet');
+  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ma-carte-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, nom-de-votre-projet');
   });
 });
